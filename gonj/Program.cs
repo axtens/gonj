f37eaf8a23e20b54bd439d117eab0c95765eef85
Bruce M. Axtens <bugmagnet@outlook.com>
2020-07-01 21:51:57 +0800

    initial

--- gonj/Program.cs
+++ gonj/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace gonj
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
+            MessageBox.Show(string.Join(" ", args.ToArray()));
         }
     }
 }
